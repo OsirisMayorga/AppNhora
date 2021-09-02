c07014190e93274ee8985679b84e06437efbba1e
Osiris <oz.jhovana06@gmail.com>
2021-09-02 16:06:45 -0500

    ticket y reservacion

--- www/js/functions.js
+++ www/js/functions.js
@@ -2,18 +2,20 @@ window.app = new Vue({
   el: '#reservas',
   data: {
     message: 'Hello Vue!',
-    steep: 0,
+    steep: 0
   },
   methods:{
     initApp:function()
     {
       console.log("Hola!");
       this.steep = 1;
+    },
+    cancelarAlert:function(){
+      alert("Hello! I am an alert box!");
     }
   },
   mounted: function () {
-    // this.initApp();
-   
+    // this.initApp();   
   }
 
 
